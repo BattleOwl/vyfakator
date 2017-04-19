4727184783e74ffab8875ff0cfd5315c3a720167
Tomas Marak <tomas.marak@i-projekt.cz>
2017-04-19 15:36:16 +0200

    šablony angular

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,6 @@
-app.controller('clickAndFuck', function($scope) {
+app.controller('ctrlClickAndFuck', function($scope) {
     $scope.firstname = "Klikni a řeknu ti co právě děláš!";
     $scope.changeName = function() {
-        $scope.firstname = "Marníš tu čas demente, tak si poklikej na tláčo";
+        $scope.firstname = "Marníš tu čas demente...";
     }
 });
